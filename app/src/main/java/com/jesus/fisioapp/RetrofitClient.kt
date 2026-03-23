5254989357744c5a093a59c16b96217b3e7a6be8
jesusgarod <jesusgarcrod@gmail.com>
2026-03-23 15:57:16 +0100

    <feat>:<añadido icono app>

--- app/src/main/java/com/jesus/fisioapp/RetrofitClient.kt
+++ app/src/main/java/com/jesus/fisioapp/RetrofitClient.kt
@@ -7,6 +7,7 @@ object RetrofitClient {
 
     //Para conectar con el PC
     private const val BASE_URL = "http://10.0.2.2:8080"
+//    private const val BASE_URL = "http://192.168.0.13:8080"
 
     val apiService: FisioApiService by lazy {
         Retrofit.Builder()
